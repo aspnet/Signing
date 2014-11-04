13eb5443acb7519ab98a5169f62d0d9be1dfb65a
anurse <andrew@andrewnurse.net>
2014-11-03 17:14:16 -0800

    Initial Checkin

---
+++ src/PackageSigning/FileSignature.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+
+namespace PackageSigning
+{
+    public class FileSignature
+    {
+        private SignedXml _signedXml;
+
+        public FileSigner Signer { get; private set; }
+
+        private FileSignature(SignedXml signedXml)
+        {
+            _signedXml = signedXml;
+
+            // Load the signer
+            var x509data = signedXml.KeyInfo.OfType<KeyInfoX509Data>().FirstOrDefault();
+            Signer = FileSigner.FromX509Data(x509data);
+        }
+
+        public static FileSignature Sign(MemoryStream file, X509Certificate2 cert)
+        {
+            var signedXml = new SignedXml();
+
+            // Create a reference to the signature file to sign
+            signedXml.AddReference(new Reference(file) {
+                DigestMethod = "http://www.w3.org/2001/04/xmlenc#sha256"
+            });
+
+            // Create a signer info for the signature
+            signedXml.SigningKey = cert.PrivateKey;
+            signedXml.KeyInfo.AddClause(new KeyInfoX509Data(cert));
+
+            // Sign the content
+            signedXml.ComputeSignature();
+
+            // Load the signature in to the object
+            return new FileSignature(signedXml);
+        }
+    }
+}
---
+++ src/PackageSigning/FileSigner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+
+namespace PackageSigning
+{
+    public class FileSigner
+    {
+        public string Subject { get; private set; }
+        public string Spki { get; private set; }
+        public IEnumerable<X509Certificate2> Certificates { get; private set; }
+        public IEnumerable<FileSigner> CounterSigners { get; private set; }
+
+        private FileSigner(string subject, string spki, IEnumerable<X509Certificate2> certificates)
+        {
+            Subject = subject;
+            Spki = spki;
+            Certificates = certificates;
+        }
+
+        internal static FileSigner FromX509Data(KeyInfoX509Data x509data)
+        {
+            var certificates = x509data.Certificates.Cast<X509Certificate2>();
+            var subjectCert = certificates.FirstOrDefault();
+            var keyIdentifierExtension = subjectCert?.Extensions?.OfType<X509SubjectKeyIdentifierExtension>()?.FirstOrDefault();
+            return new FileSigner(
+                subjectCert?.Subject,
+                keyIdentifierExtension?.SubjectKeyIdentifier,
+                certificates);
+        }
+    }
+}
